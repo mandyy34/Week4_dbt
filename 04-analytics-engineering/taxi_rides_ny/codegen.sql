38f3791b5c9ac49000764d1504d5f9a4fa4c53ff
Mandy Yau <ymyau6@gmail.com>
2024-02-21 07:49:30 +0000

    update codegen

--- 04-analytics-engineering/taxi_rides_ny/codegen.sql
+++ 04-analytics-engineering/taxi_rides_ny/codegen.sql
@@ -1,4 +1,4 @@
-{% set models_to_generate = codegen.get_models(directory='core', prefix='fact_fhv') %}
+{% set models_to_generate = codegen.get_models(directory='core', prefix='fact_fhv_') %}
 {{ codegen.generate_model_yaml(
     model_names = models_to_generate
 ) }}
